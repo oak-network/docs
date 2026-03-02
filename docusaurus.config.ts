d919622e772b980761143d73f93bd04ffffc9d27
fahmidareem3 <fahmidareem3@gmail.com>
2026-03-02 20:41:49 +0600

    fix: update github link and link colours

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -156,7 +156,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/oak-network/ccprotocol-contracts',
+          href: 'https://github.com/oak-network',
           label: 'GitHub',
           position: 'right',
         },
